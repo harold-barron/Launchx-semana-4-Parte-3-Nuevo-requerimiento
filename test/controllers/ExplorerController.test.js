7e5469f55a57d909a0e3ba755170c0c0a569e3af
Harold Barron <haroldbarron13@gmail.com>
2022-04-27 23:17:46 -0500

    Updating files

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,22 @@
+const ExplorerController = require ("./../../lib/controllers/ExplorerController");
+
+describe("Test for Explorer Controller", ()=>
+{
+    test("1. Gettin de list of explorers with a specific mission",()=>
+    {
+        const listOfExplorers = ExplorerController.getExplorersByMission("node");
+        const ExplorersWithNode = listOfExplorers.forEach(expName => expect(expName.mission).toContain("node"));
+    });
+    test("2. Gettin de names of the explorers in a specific mission",()=>
+    {
+        const namesOfExplorers = ExplorerController.getExplorersUsernamesByMission("node");
+        console.log(namesOfExplorers);   
+    });
+    test("3. Gettin de amount of the explorers in a specific mission",()=>
+    {
+        const amountOfExplorers = ExplorerController.getExplorersAmonutByMission("node");
+        expect(amountOfExplorers).toBe(10);
+    });
+});
+
+
